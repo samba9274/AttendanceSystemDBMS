e081697ff357dcb0d2e3a5e51337102fd14b4efb
Yash Eksambekar <yasheksambekar@gmail.com>
2021-11-23 09:10:52 +0530

    added db source file

--- express/server.js
+++ express/server.js
@@ -28,34 +28,36 @@ connection.connect();
 app.get("/", (req, res) => res.send("Server ready"));
 
 app.get("/reset", (req, res) => {
-  connection.query(
-    "DROP TABLES classes, students, teachers, subjects, lectures, attendance",
-    (error, results, fields) => {}
-  );
-  connection.query(
-    "CREATE TABLE classes (class_id INT PRIMARY KEY AUTO_INCREMENT,class_name VARCHAR(20))",
-    (error, results, fields) => {}
-  );
-  connection.query(
-    "CREATE TABLE students (student_id INT PRIMARY KEY AUTO_INCREMENT,student_name VARCHAR(30),student_roll VARCHAR(20) UNIQUE,student_password VARCHAR(8),class_id INT,FOREIGN KEY (class_id) REFERENCES classes(class_id))",
-    (error, results, fields) => {}
-  );
-  connection.query(
-    "CREATE TABLE teachers (teacher_id INT PRIMARY KEY AUTO_INCREMENT,teacher_name VARCHAR(30),teacher_username VARCHAR(15) UNIQUE,teacher_password VARCHAR(8))",
-    (error, results, fields) => {}
-  );
-  connection.query(
-    "CREATE TABLE subjects (subject_id INT PRIMARY KEY AUTO_INCREMENT,subject_name VARCHAR(15),teacher_id INT,class_id INT,FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id),FOREIGN KEY (class_id) REFERENCES classes(class_id))",
-    (error, results, fields) => {}
-  );
-  connection.query(
-    "CREATE TABLE lectures (lecture_id INT PRIMARY KEY AUTO_INCREMENT,lecture_name VARCHAR(30),lecture_code VARCHAR(5) UNIQUE,lecture_start DATETIME,subject_id INT,FOREIGN KEY (subject_id) REFERENCES subjects(subject_id))",
-    (error, results, fields) => {}
-  );
-  connection.query(
-    "CREATE TABLE attendance (attendance_id INT PRIMARY KEY AUTO_INCREMENT,student_id INT,lecture_id INT,FOREIGN KEY (student_id) REFERENCES students(student_id),FOREIGN KEY (lecture_id) REFERENCES lectures(lecture_id))",
-    (error, results, fields) => {}
-  );
+  connection.query("SOURCE /db/db.sql", (error, results, fields) => {});
+
+  // connection.query(
+  //   "DROP TABLES classes, students, teachers, subjects, lectures, attendance",
+  //   (error, results, fields) => {}
+  // );
+  // connection.query(
+  //   "CREATE TABLE classes (class_id INT PRIMARY KEY AUTO_INCREMENT,class_name VARCHAR(20))",
+  //   (error, results, fields) => {}
+  // );
+  // connection.query(
+  //   "CREATE TABLE students (student_id INT PRIMARY KEY AUTO_INCREMENT,student_name VARCHAR(30),student_roll VARCHAR(20) UNIQUE,student_password VARCHAR(8),class_id INT,FOREIGN KEY (class_id) REFERENCES classes(class_id))",
+  //   (error, results, fields) => {}
+  // );
+  // connection.query(
+  //   "CREATE TABLE teachers (teacher_id INT PRIMARY KEY AUTO_INCREMENT,teacher_name VARCHAR(30),teacher_username VARCHAR(15) UNIQUE,teacher_password VARCHAR(8))",
+  //   (error, results, fields) => {}
+  // );
+  // connection.query(
+  //   "CREATE TABLE subjects (subject_id INT PRIMARY KEY AUTO_INCREMENT,subject_name VARCHAR(15),teacher_id INT,class_id INT,FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id),FOREIGN KEY (class_id) REFERENCES classes(class_id))",
+  //   (error, results, fields) => {}
+  // );
+  // connection.query(
+  //   "CREATE TABLE lectures (lecture_id INT PRIMARY KEY AUTO_INCREMENT,lecture_name VARCHAR(30),lecture_code VARCHAR(5) UNIQUE,lecture_start DATETIME,subject_id INT,FOREIGN KEY (subject_id) REFERENCES subjects(subject_id))",
+  //   (error, results, fields) => {}
+  // );
+  // connection.query(
+  //   "CREATE TABLE attendance (attendance_id INT PRIMARY KEY AUTO_INCREMENT,student_id INT,lecture_id INT,FOREIGN KEY (student_id) REFERENCES students(student_id),FOREIGN KEY (lecture_id) REFERENCES lectures(lecture_id))",
+  //   (error, results, fields) => {}
+  // );
   res.status(200).send();
 });
 
---
+++ requests.sh
@@ -0,0 +1,6 @@
+curl -X POST -H "Content-Type: application/json" -d '' https://example/contact
+
+
+curl -X POST -H "Content-Type: application/json" -d '{"subject": {"name": "DBMS","teacher": 1,"class": 1}}' https://example/contact
+curl -X POST -H "Content-Type: application/json" -d '{"class": {"name": "TE Shift 3"}}' https://example/contact
+curl -X POST -H "Content-Type: application/json" -d '' https://example/contact
